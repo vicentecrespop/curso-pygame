65de5e3b0e78cde64d435a3812bc9019c774909f
vicentecrespop <vicentecrespo45@gmail.com>
2023-03-15 10:37:36 -0300

    app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 pygame.init()
 
@@ -8,6 +9,9 @@ largura = 640
 altura = 480
 x = largura / 2
 y = altura / 2
+
+x_azul = randint(40, 600)
+y_azul = randint(50, 430)
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Primeiro Jogo')
@@ -38,7 +42,13 @@ while True:
         y = y - 20
     if pygame.key.get_pressed()[K_s]:
         y = y + 20
-    pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
+
+    ret_vermelho = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
+    ret_azul = pygame.draw.rect(tela, (0, 0, 255), (x_azul, y_azul, 40, 50))
+
+    if ret_vermelho.colliderect(ret_azul):
+        x_azul = randint(40, 600)
+        y_azul = randint(50, 430)
     
 
     pygame.display.update()
