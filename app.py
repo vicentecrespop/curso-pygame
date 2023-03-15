220fb23a56c98171466202b3af30a081387b6c30
vicentecrespop <vicentecrespo45@gmail.com>
2023-03-15 10:31:40 -0300

    app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ pygame.init()
 largura = 640
 altura = 480
 x = largura / 2
-y = 0
+y = altura / 2
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Primeiro Jogo')
@@ -20,12 +20,26 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+        # if event.type == KEYDOWN:
+        #     if event.key == K_a:
+        #         x = x - 20
+        #     if event.key == K_d:
+        #         x = x + 20
+        #     if event.key == K_w:
+        #         y = y - 20
+        #     if event.key == K_s:
+        #         y = y + 20
         
-
+    if pygame.key.get_pressed()[K_a]:
+        x = x - 20
+    if pygame.key.get_pressed()[K_d]:
+        x = x + 20
+    if pygame.key.get_pressed()[K_w]:
+        y = y - 20
+    if pygame.key.get_pressed()[K_s]:
+        y = y + 20
     pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
-    if y >= altura:
-        y = 0
-    y = y + 1
+    
 
     pygame.display.update()
     
