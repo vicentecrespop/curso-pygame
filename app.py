b5050f1a348174ef2155861b2038c3575712cd77
vicentecrespop <vicentecrespo45@gmail.com>
2023-03-15 14:15:26 -0300

    app

--- app.py
+++ app.py
@@ -1,147 +1,72 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from random import randint
 
 pygame.init()
 
-pygame.mixer.music.set_volume(0.05)
-musica_de_fundo = pygame.mixer.music.load('BoxCat Games - Victory.mp3')
-pygame.mixer.music.play(-1)
-
-barulho_colisao = pygame.mixer.Sound('smw_coin.wav')
-barulho_colisao.set_volume(1)
-
 largura = 640
 altura = 480
-x_cobra = largura / 2
-y_cobra = altura / 2
 
-velocidade = 5
-x_controle = velocidade
-y_controle = 0
-
-x_maca = randint(40, 600)
-y_maca = randint(50, 430)
-
-pontos = 0
-fonte = pygame.font.SysFont('arial', 40, True)
+PRETO = (0, 0, 0)
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Primeiro Jogo')
+pygame.display.set_caption('Sprites')
+
+class Sapo(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('sprites/attack_1.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_2.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_3.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_4.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_5.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_6.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_7.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_8.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_9.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_10.png'))
+        self.atual = 0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (128*3, 64*3))
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 100, 100
+
+        self.animar = False
+
+    def update(self):
+        if self.animar == True:
+            self.atual = self.atual + 0.5
+            if self.atual >= len(self.sprites):
+                self.atual = 0
+                self.animar = False
+            self.image = self.sprites[int(self.atual)]
+            self.image = pygame.transform.scale(self.image, (128*3, 64*3))
+
+    def atacar(self):   
+        self.animar = True
+
+
+
+todas_as_sprites = pygame.sprite.Group()
+sapo = Sapo()
+todas_as_sprites.add(sapo)
+
 relogio = pygame.time.Clock()
-lista_cobra = []
-comprimento_inicial = 10
-morreu = False
-
-
-def aumenta_cobra(lista_cobra):
-    for XeY in lista_cobra:
-        pygame.draw.rect(tela, (0, 255, 0), (XeY[0], XeY[1], 20, 20))
-
-def reiniciar_jogo():
-    global pontos, comprimento_inicial, x_cobra, y_cobra, lista_cobra, lista_cabeca, x_maca, y_maca, morreu
-    pontos = 0
-    comprimento_inicial = 5
-    x_cobra = largura / 2
-    y_cobra = altura / 2
-    lista_cobra = []
-    lista_cabeca = []
-    x_maca = randint(40, 600)
-    y_maca = randint(50, 430)
-    morreu = False
-
 
 while True:
-    relogio.tick(60)
-    tela.fill((255 , 255, 255))
-    mensagem = f'Pontos: {pontos}'
-    texto_formatado = fonte.render(mensagem, True, (0, 0, 0))
+
+    relogio.tick(30)
+    tela.fill(PRETO)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
         if event.type == KEYDOWN:
-            if event.key == K_a:
-                if x_controle == velocidade:
-                    pass
-                else:
-                    x_controle = - velocidade
-                    y_controle = 0
-            if event.key == K_d:
-                if x_controle == - velocidade:
-                    pass
-                else:
-                    x_controle = velocidade
-                    y_controle = 0
-            if event.key == K_w:
-                if y_controle == velocidade:
-                    pass
-                else:
-                    y_controle = - velocidade
-                    x_controle = 0
-            if event.key == K_s:
-                if y_controle == - velocidade:
-                    pass
-                else:
-                    y_controle = velocidade
-                    x_controle = 0
-        
-    x_cobra = x_cobra + x_controle
-    y_cobra = y_cobra + y_controle
+            sapo.atacar()
 
-    cobra = pygame.draw.rect(tela, (0, 255, 0), (x_cobra, y_cobra, 20, 20))
-    maca = pygame.draw.rect(tela, (255, 0, 0), (x_maca, y_maca, 20, 20))
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
 
-    if cobra.colliderect(maca):
-        x_maca = randint(40, 600)
-        y_maca = randint(50, 430)
-        pontos = pontos + 1
-        barulho_colisao.play()
-        comprimento_inicial = comprimento_inicial + 1
-
-    lista_cabeca = []
-    lista_cabeca.append(x_cobra)
-    lista_cabeca.append(y_cobra)
-
-    lista_cobra.append(lista_cabeca)
-
-    if lista_cobra.count(lista_cabeca) > 1:
-        fonte2 = pygame.font.SysFont('arial', 20, True, True)
-        mensagem = 'Game Over! Pressione a tecla R para jogar novamente'
-        texto_formatado = fonte2.render(mensagem, True, (0, 0, 0))
-        ret_texto = texto_formatado.get_rect()
-        morreu = True
-        while morreu:
-            tela.fill((255, 255, 255))
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    exit()
-                if event.type == KEYDOWN:
-                    if event.key == K_r:
-                        reiniciar_jogo()
-            ret_texto.center = (largura//2, altura//2)
-            tela.blit(texto_formatado, ret_texto)
-            pygame.display.update()
-
-    
-    if x_cobra > largura:
-        x_cobra = 0
-    if x_cobra < 0:
-        x_cobra = largura
-    if y_cobra < 0:
-        y_cobra = altura
-    if y_cobra > altura:
-        y_cobra = 0
-
-
-    if len(lista_cobra) > comprimento_inicial:
-        del lista_cobra[0]
-
-    aumenta_cobra(lista_cobra)
-
-    tela.blit(texto_formatado, (420, 40))
-
-    pygame.display.update()
-    
+    pygame.display.flip()
